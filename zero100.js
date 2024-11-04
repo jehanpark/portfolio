8c77cd2610a277a56c53eb7566888fda76de95c0
jehanpark <5071pjh@naver.com>
2024-11-04 18:43:28 +0900

    241104

--- zero100.js
+++ zero100.js
@@ -1,1 +1,33 @@
+const wfSlide = document.querySelector(".wfSlide");
+const imgs = Array.from(wfSlide.children);
 
+imgs.forEach((img) => {
+  const clone = img.cloneNode(true);
+  wfSlide.appendChild(clone);
+});
+
+["mouseenter", "mouseleave"].forEach((event) => {
+  wfSlide.addEventListener(event, () => {
+    wfSlide.style.animationPlayState =
+      event === "mouseenter" ? "paused" : "running";
+  });
+});
+
+const animatedElements = document.querySelectorAll(
+  ".lefttImg, .rightImg, .info, .flow, .homeImg"
+);
+
+// Intersection Observer 생성
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.8 }
+);
+
+animatedElements.forEach((element) => observer.observe(element));
